cb1522993725799173437f78cce2cb2fc4bef9c0
Jomon Johnson <jomon.entero@gmail.com>
2019-03-22 19:58:45 +0530

    init

---
+++ config/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 16/1/19
+ * Time: 5:34 PM
+ */
+
+return [
+    'components' => [
+        'settings' => [
+            'class' => \codexten\yii\settings\components\Settings::class,
+        ],
+    ],
+];
